a99ef73d0c98a71f622d9f702a411fea28b02444
raquelgm88 <raquel.rgm7@gmail.com>
2023-06-19 13:18:43 +0200

    Layout only to take a screenshot

--- src/components/App.js
+++ src/components/App.js
@@ -58,13 +58,15 @@ function App() {
       <img className="header__logo" src={logo} alt="Logo Family Shopping" />
     </header>
     <main className="main">
-      <h2 className="main__h2">Bienvenido a</h2>
-      <h1 className="main__h1">FAMILY SHOPPING</h1>
-      <p className="main__p">
-        Descubre los mejores centros comerciales de la Comunidad de Madrid para
-        ir de compras con tus hijos
-      </p>
-      <button className="main__button">Empezar</button>
+      <div className='main__container'>
+        <h2 className="main__container-h2">Bienvenido a</h2>
+        <h1 className="main__container-h1">FAMILY SHOPPING</h1>
+        <p className="main__container-p">
+          Descubre los mejores centros comerciales de la Comunidad de Madrid para
+          ir de compras con tus hijos
+        </p>
+        <button className="main__container-button">Empezar</button>
+      </div>
     </main>
     <footer className="footer">
       <p className="footer__p"></p>
